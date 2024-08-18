68ba5b9399e1fc904895509bfbefac4cbc5aaca1
Asher Nzurum <theforce289@gmail.com>
2024-08-18 01:54:01 +0100

    feat: add login page

--- app/src/main/java/com/example/recipeease/MainActivity.kt
+++ app/src/main/java/com/example/recipeease/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.recipeease
 
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -24,6 +25,7 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -52,11 +54,20 @@ import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.painter.Painter
@@ -84,8 +95,10 @@ fun MultiplePages(modifier: Modifier = Modifier) {
 
     NavHost(navController, startDestination = "page1") {
         composable("page1") { WelcomePage(navController = navController) }
-        composable("page2") { HomePage(navController = navController) }
+        composable("page2") { LoginPage(navController = navController)}
+        composable("page3") { HomePage(navController = navController) }
 //        composable("page3") { Page3() }
+        composable("page5"){ RecipesPage(navController = navController)}
     }
 }
 
@@ -99,10 +112,153 @@ fun WelcomePage(modifier: Modifier = Modifier, navController: NavController){
 }
 
 @Composable
+fun LoginPage(modifier: Modifier = Modifier, navController: NavController){
+    val email by remember {
+        mutableStateOf("")
+    }
+
+    val password by remember {
+        mutableStateOf("")
+    }
+
+    Column(modifier = modifier
+        .fillMaxSize()
+        .verticalScroll(state = rememberScrollState(), enabled = true)
+        .padding(20.dp), verticalArrangement = Arrangement.Center) {
+        Row(modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            Image(painter = painterResource(id = R.drawable.pixlr_image_generator_244912cb_80d1_44d2_8a05_5f0528b2a80a), contentDescription = null, modifier = modifier.size(110.dp))
+        }
+        Row(modifier = modifier
+            .fillMaxWidth()
+            .padding(top = 20.dp), horizontalArrangement = Arrangement.Center){
+            Text(text = stringResource(id = R.string.recipe_ease), color = Color.White, fontWeight = FontWeight.Bold, fontSize = 24.sp)
+        }
+        Row(modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center){
+            Text(text = "Discover recipes effortlessly!", color = Color.White, fontSize = 20.sp)
+        }
+        Row(modifier = modifier.padding(top = 20.dp)) {
+            MyTextField(textValue = email, type = "Email", modifier = modifier.clip(shape = RoundedCornerShape(32.dp)))
+        }
+        Row(modifier = modifier.padding(top = 20.dp)) {
+            MyTextField(
+                textValue = password,
+                type = "Password",
+                modifier = modifier.clip(shape = RoundedCornerShape(32.dp))
+            )
+        }
+        Row(modifier = modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp), horizontalArrangement = Arrangement.End) {
+            Text(text = "Forgot your password?", color = Color.White)
+        }
+        Row(modifier = modifier.fillMaxWidth()) {
+            LoginButton(navController = navController)
+        }
+        Row(modifier = modifier
+            .fillMaxWidth()
+            ) {
+
+            Row (modifier = modifier.fillMaxWidth(0.48f).fillMaxHeight().padding(10.dp)){
+                HorizontalDivider(modifier = modifier
+                    .weight(3f).align(Alignment.CenterVertically))
+            }
+
+           Row(modifier = modifier.fillMaxWidth(0.12f)) {
+               Text(text = "or", modifier = modifier
+                   .align(Alignment.CenterVertically), color = Color.White, fontSize = 20.sp)
+           }
+            Row (modifier = modifier.fillMaxWidth(1f).fillMaxHeight().padding(10.dp)){
+                HorizontalDivider(modifier = modifier
+                    .weight(3f)
+                    .align(Alignment.CenterVertically))
+            }
+        }
+        Row(modifier = modifier.fillMaxWidth().padding(top = 20.dp)) {
+            AuthButtons(modifier = modifier.weight(1f),text = "Google")
+            AuthButtons(modifier = modifier.weight(1f),text = "Facebook")
+        }
+        Row(modifier = modifier
+            .fillMaxWidth()
+            .padding(top = 20.dp), horizontalArrangement = Arrangement.Center) {
+            Text(text = "New here? ", color = Color.White, fontWeight = FontWeight.Bold)
+            Text(text = "Join Now", color = Color.White, fontWeight = FontWeight.Bold)
+        }
+    }
+}
+
+@Composable
+fun MyTextField(modifier: Modifier = Modifier, textValue: String, type: String){
+    var value by remember {
+        mutableStateOf(textValue)
+    }
+
+    TextField(value = value, onValueChange = {value = it}, singleLine = true, colors = TextFieldDefaults.colors(focusedTextColor = Color.White, unfocusedTextColor = Color.White,unfocusedContainerColor = Color(0xFF2b2b2b), focusedContainerColor = Color(0xFF2b2b2b), focusedIndicatorColor = Color(0xFF2b2b2b), unfocusedIndicatorColor = Color(0xFF2b2b2b)),placeholder = { Text(text = type, color = Color.White)},modifier = modifier
+        .fillMaxWidth())
+}
+
+@Composable
+fun LoginButton(modifier: Modifier = Modifier, navController: NavController){
+    Button(onClick = { login(navController)}, modifier = modifier
+        .fillMaxWidth()
+        .padding(vertical = 30.dp), colors = ButtonColors(contentColor = Color.White, disabledContentColor = Color.Black, containerColor = Color(0xFF2b2b2b), disabledContainerColor = Color.White)) {
+        Text(text = "Log in", fontSize = 20.sp, fontWeight = FontWeight.Bold, modifier = modifier.padding(4.dp))
+    }
+}
+
+@Composable
+fun AuthButtons(modifier: Modifier = Modifier, text: String){
+    Button(onClick = { }, modifier = modifier
+        .fillMaxWidth()
+        .padding(horizontal = 10.dp), colors = ButtonColors(contentColor = Color.White, disabledContentColor = Color.Black, containerColor = Color.Black, disabledContainerColor = Color.White)) {
+        Text(text = text, fontSize = 18.sp, modifier = modifier.padding(4.dp), textAlign = TextAlign.Center)
+    }
+}
+
+fun login(navController: NavController){
+    navController.navigate("page3")
+}
+
+@Composable
 fun HomePage(modifier: Modifier = Modifier, navController: NavController){
     Column(modifier.fillMaxSize()) {
         ScrollableHomePage(modifier = modifier.verticalScroll(state = ScrollState(0),enabled = true), navController = navController)
         Footer(navController = navController)
+    }
+}
+
+@Composable
+fun RecipesPage(modifier: Modifier = Modifier, navController: NavController){
+    Column {
+        RecipesHeader(navController = navController,
+            modifier = modifier
+                .height(110.dp)
+                .background(Color(0xFF2b2b2b)))
+        RecipeList()
+        Footer(navController = navController)
+    }
+}
+
+@Composable
+fun RecipesHeader(modifier: Modifier = Modifier, navController: NavController){
+    Column(modifier = modifier
+        .fillMaxSize()
+        .padding(top = 20.dp, start = 20.dp)) {
+        Row (modifier = modifier){
+            Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Back button", tint = Color.White, modifier = modifier
+                .padding(vertical = 30.dp)
+                .clickable { navController.navigateUp() })
+            Text(text = "Recipes", fontWeight = FontWeight.Bold, color = Color.White, fontSize = 24.sp, modifier = modifier.padding(30.dp))
+        }
+        Row {
+
+        }
+    }
+}
+
+@Composable
+fun RecipeList(modifier: Modifier = Modifier){
+    Column(modifier = modifier.verticalScroll(state = rememberScrollState(), enabled = true)) {
+
     }
 }
 
@@ -168,7 +324,14 @@ fun DiscoverCard(modifier: Modifier = Modifier){
 fun CardFront(modifier: Modifier = Modifier){
     val images = arrayOf(painterResource(id = R.drawable.nik_6hzhfuivo60_unsplash), painterResource(id = R.drawable.nik_6hzhfuivo60_unsplash), painterResource(id = R.drawable.nik_6hzhfuivo60_unsplash))
 
-    Box (modifier = modifier.padding(top = 20.dp).shadow(elevation = 5.dp, spotColor = Color.Black, ambientColor = Color.White, shape = RoundedCornerShape(16.dp))){
+    Box (modifier = modifier
+        .padding(top = 20.dp)
+        .shadow(
+            elevation = 5.dp,
+            spotColor = Color.Black,
+            ambientColor = Color.White,
+            shape = RoundedCornerShape(16.dp)
+        )){
         Box(modifier = modifier
             .clip(RoundedCornerShape(16.dp))
             .fillMaxWidth()
@@ -196,8 +359,17 @@ fun CardFront(modifier: Modifier = Modifier){
 
 @Composable
 fun CardBack(modifier: Modifier = Modifier){
-    Box(modifier = modifier.fillMaxHeight().fillMaxWidth(0.95f).padding(top = 20.dp).offset(x = 8.dp).clip(RoundedCornerShape(16.dp))){
-        Box(modifier.background(color = Color(0xFF2b2b2b)).fillMaxWidth().size(180.dp)){
+    Box(modifier = modifier
+        .fillMaxHeight()
+        .fillMaxWidth(0.95f)
+        .padding(top = 20.dp)
+        .offset(x = 8.dp)
+        .clip(RoundedCornerShape(16.dp))){
+        Box(
+            modifier
+                .background(color = Color(0xFF2b2b2b))
+                .fillMaxWidth()
+                .size(180.dp)){
 
         }
     }
@@ -267,7 +439,7 @@ fun Footer(modifier: Modifier = Modifier, navController: NavController){
             .fillMaxWidth()
             .background(color = Color(0xFF2b2b2b))
             .padding(vertical = 10.dp), horizontalArrangement = Arrangement.SpaceEvenly){
-            Column(modifier = modifier.clickable {navController.navigate("page2") }, horizontalAlignment = Alignment.CenterHorizontally){
+            Column(modifier = modifier.clickable {navController.navigate("page3") }, horizontalAlignment = Alignment.CenterHorizontally){
                 Icon(imageVector = Icons.Filled.Home, contentDescription = null, modifier, tint = Color.White)
                 Text(text = stringResource(R.string.home), color = Color.White, textAlign = TextAlign.Center)
             }
@@ -275,9 +447,9 @@ fun Footer(modifier: Modifier = Modifier, navController: NavController){
                 Icon(imageVector = Icons.Filled.Search, contentDescription = null, modifier, tint = Color.White)
                 Text(text = "Search", color = Color.White, textAlign = TextAlign.Center)
             }
-            Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally){
+            Column(modifier = modifier.clickable { navController.navigate("page5") }, horizontalAlignment = Alignment.CenterHorizontally){
                 Icon(imageVector = Icons.Filled.List, contentDescription = null, modifier, tint = Color.White)
-                Text(text = "List", color = Color.White, textAlign = TextAlign.Center)
+                Text(text = "Recipes", color = Color.White, textAlign = TextAlign.Center)
             }
             Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally){
                 Icon(imageVector = Icons.Filled.AccountCircle, contentDescription = null, modifier, tint = Color.White)
